b86e7ea277651dbf59a24980ec5eeb54057d360c
Josh Frye <jfrye@uoregon.edu>
2017-04-10 20:37:46 -0700

    Multi-file parser now skips files that don't exist

--- data/CPNWH_MultiFileParser.py
+++ data/CPNWH_MultiFileParser.py
@@ -6,6 +6,7 @@ import numpy as np
 
 import argparse
 
+
 # Parse arguments
 parser = argparse.ArgumentParser(
     description='Parse CPNWH multiple data files')
@@ -24,14 +25,20 @@ num_files = args.number_of_files
 # Read multiple search text files into datasets
 folder = 'CPNWH_' + region + '/'
 file_prefix = "all_species"
-datasets = [
-    pd.read_csv(
-        './' + folder + 'raw_data/' + \
-        file_prefix + "_raw_data" + str(i) + ".txt", sep="\t",
-        usecols=["Genus", "Specific Epithet", "Specimen Count"]
-        )
-    for i in range(1, num_files)
-    ]
+datasets = []
+for i in range(num_files):
+    input_file_name = file_prefix + "_raw_data" + str(i) + ".txt"
+    input_file_path = './' + folder + 'raw_data/' + input_file_name
+    print("Parsing " + input_file_name + " ...")
+    try:
+        df = pd.read_csv(
+            input_file_path, sep="\t",
+            usecols=["Genus", "Specific Epithet", "Specimen Count"]
+            )
+    except FileNotFoundError:
+        print("File not find!")
+        continue
+    datasets.append(df)
 
 # Combine the datasets
 all_species = pd.concat(datasets, ignore_index=True)
@@ -40,7 +47,8 @@ all_species = pd.concat(datasets, ignore_index=True)
 # (e.g. when the observer could not determine it)
 # These show up as NaN; we replace these with a unique text to proceed
 all_species = all_species.replace(np.nan, "NAN")
-all_species['binomial'] = all_species['Genus'] + ' ' + all_species['Specific Epithet']
+all_species['binomial'] = \
+    all_species['Genus'] + ' ' + all_species['Specific Epithet']
 
 # The dataframe is degenerate; this combines those counts
 species_counts = all_species.groupby('binomial')['Specimen Count'] \
