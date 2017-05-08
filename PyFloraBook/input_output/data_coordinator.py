18aceb22db90400aa3f4f0b9da45a25bcf6aaaf9
Josh Frye <jfrye@uoregon.edu>
2017-05-08 13:28:52 -0700

    Create initial project configuration system

---
+++ PyFloraBook/input_output/data_coordinator.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import json
+import inspect
+
+import PyFloraBook
+
+
+def load_configuration() -> dict:
+    """Load project configuration info
+
+    Returns:
+        Dictionary of configuration info.
+    """
+    source_path = Path(inspect.getsourcefile(PyFloraBook)).parent
+    # This assumes that the PyFloraBook __init__ file is contained in a
+    # sub-folder of the project path
+    project_path = source_path.parent
+    configuration = project_path / "configuration.json"
+    with configuration.open() as config_file:
+        return json.load(config_file)
+
+
+def locate_data_folder() -> Path:
+    """Locate folder for project data
+
+    Returns:
+        Path of the data folder
+    """
+    return load_configuration()["data_folder"]
