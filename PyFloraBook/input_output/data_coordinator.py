1f747f7d9b4ebe84da1c7a8dc8d335508d99fde4
Josh Frye <jfrye@uoregon.edu>
2017-05-09 13:39:49 -0700

    Add function for obtaining project folder; implement in setup.py

--- PyFloraBook/input_output/data_coordinator.py
+++ PyFloraBook/input_output/data_coordinator.py
@@ -5,25 +5,24 @@ import inspect
 import PyFloraBook
 
 
+def locate_project_folder() -> Path:
+    """Locate top-level project folder
+
+    Returns:
+        Path of the project folder
+    """
+    source_path = Path(inspect.getsourcefile(PyFloraBook)).parent
+    # This assumes that the highest-level project __init__ file is contained
+    # in a sub-folder of the project folder
+    return source_path.parent
+
+
 def load_configuration() -> dict:
     """Load project configuration info
 
     Returns:
         Dictionary of configuration info.
     """
-    source_path = Path(inspect.getsourcefile(PyFloraBook)).parent
-    # This assumes that the PyFloraBook __init__ file is contained in a
-    # sub-folder of the project path
-    project_path = source_path.parent
-    configuration = Path(project_path / "configuration.json")
+    configuration = Path(locate_project_folder() / "configuration.json")
     with configuration.open() as config_file:
         return json.load(config_file)
-
-
-def locate_data_folder() -> Path:
-    """Locate folder for project data
-
-    Returns:
-        Path of the data folder
-    """
-    return load_configuration()["data_folder"]
--- setup.py
+++ setup.py
@@ -1,30 +1,31 @@
-import sys
+from pathlib import Path
 import site
 
+import PyFloraBook.input_output.data_coordinator as dc
 
-PROJECT_FOLDER = "/"
 
-
-def main():
+def add_project_to_pythonpath():
     package_directories = site.getsitepackages()
     site_packages = [x for x in package_directories if 'site-packages' in x]
     if len(site_packages) == 0:
         raise ValueError("No site-package directory found!")
     elif len(site_packages) == 1:
-        site_package_directory = site_packages[0]
+        site_package_directory = Path(site_packages[0])
     else:
         raise ValueError("Multiple site-packages found!")
 
-    custom_site_file = \
-        site_package_directory + PROJECT_FOLDER + "PyFloraBook.pth"
-    source_directory = sys.path[0] + PROJECT_FOLDER
-    print("Writing source directory ({}) to custom *.pth file:\n\n{}".format(
-        '.' + PROJECT_FOLDER, custom_site_file
-        ))
-    with open(custom_site_file, 'w') as pth_file:
-        pth_file.write(source_directory)
+    project_site_package = site_package_directory / "PyFloraBook.pth"
+    project_directory = dc.locate_project_folder()
+    with project_site_package.open(mode='w') as pth_file:
+        pth_file.write(str(project_directory))
 
-    print("\nDONE!")
+    print("Wrote project directory to custom *.pth file:\n\n{}".format(
+          project_site_package))
+
+
+def create_config_file():
+    pass  # TODO LEFT OFF HERE
 
 if __name__ == "__main__":
-    main()
+    add_project_to_pythonpath()
+    create_config_file()
