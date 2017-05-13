8e64733aeb068900a2bcb2a79f40afa0a1e27dda
Josh Frye <josh.n.frye@gmail.com>
2017-05-13 13:43:00 -0700

    Put url query text into a dict

--- scripts/observations/scrape/CPNWH_Downloader.py
+++ scripts/observations/scrape/CPNWH_Downloader.py
@@ -12,25 +12,13 @@ import requests
 import PyFloraBook.input_output.data_coordinator as dc
 
 
-# Parse arguments
-PARSER = argparse.ArgumentParser(
-    description='Download CPNWH data into multiple files')
-PARSER.add_argument(
-    "-r", "--region", type=str, choices=['OR', 'WA'], required=True,
-    help="Region to use."
-    )
-ARGS = PARSER.parse_args()
-REGION = ARGS.region
-
-SITE_NAME = 'CPNWH_' + REGION
-
-SCRIPT_PATH = dc.locate_current_script_folder()
-POLYGON_FILE_NAME = SITE_NAME + '_polygon.txt'
-POLYGON_FILE_PATH = SCRIPT_PATH / POLYGON_FILE_NAME
-
-POLYGON_QUERY = "&PolygonCount=1&Polygon1="
-with POLYGON_FILE_PATH.open(mode='r') as polygon_file:
-    polygon = polygon_file.read().strip()
+# Globals
+# These are URL queries
+QUERIES = {
+    "polygon": "&PolygonCount=1&Polygon1=",
+    "herbaria": "&Herbaria=",
+    "family": "&Family1=",
+}
 
 HERBARIA = [
     "ALA", "BABY", "BBLM", "BLMMD", "BOIS", "CIC", "CRMO", "EVE", "EWU", "FHL",
@@ -39,27 +27,43 @@ HERBARIA = [
     "VALE", "VIU", "WCW", "WS", "WSTC", "WTU", "WWB"
     ]
 
-HERBARIA_QUERY = "&Herbaria="
-
-# Gather components of URL
 MAIN_URL = (
     "http://www.pnwherbaria.org/data/results.php?DisplayAs=Checklist"
     "&DisplayOption=Tab&ExcludeCultivated=Y&GroupBy=ungrouped"
     "&SortBy=ScientificName&SortOrder=ASC&QueryCount=1"
     "&Zoom=7&Lat=46.71797026962876&Lng=-123.013916015625"
     )
-region_url = MAIN_URL + POLYGON_QUERY + polygon
 
-FAMILY_QUERY = "&Family1="
-
-OUTPUT_PATH = dc.locate_raw_data_folder()
 OUTPUT_FILE_PREFIX = "all_species"
+
+# Parse arguments
+PARSER = argparse.ArgumentParser(
+    description='Download CPNWH data into multiple files')
+PARSER.add_argument(
+    "-r", "--region", type=str, choices=['OR', 'WA'], required=True,
+    help="Region to use."
+    )
+args = PARSER.parse_args()
+region = args.region
+
+site_name = 'CPNWH_' + region
+# Load polygon file
+script_path = dc.locate_current_script_folder()
+polygon_file_name = site_name + '_polygon.txt'
+polygon_file_path = script_path / polygon_file_name
+with polygon_file_path.open(mode='r') as polygon_file:
+    polygon = polygon_file.read().strip()
+
+# Gather components of URL
+region_url = MAIN_URL + QUERIES["polygon"] + polygon
+
+output_path = dc.locate_raw_data_folder()
 
 unknown_error_herbaria = []
 empty_herbaria = []
 for i, herbarium in enumerate(HERBARIA):
     print(str(i) + ": \tRequesting herbarium: \t" + herbarium)
-    herbarium_url = region_url + (HERBARIA_QUERY + herbarium)
+    herbarium_url = region_url + (QUERIES["herbaria"] + herbarium)
     response = requests.get(herbarium_url)
     if response.headers['Content-Type'] == 'text/html':
         print("*** No CSV returned!")
@@ -81,19 +85,19 @@ for i, herbarium in enumerate(HERBARIA):
         print("*** Timeout confirmed.")
         print("*** Splitting requests by first letter of family name...")
         for letter in string.ascii_uppercase:
-            alphabetical_family_query = FAMILY_QUERY + letter + '%'
-            alphabetical_url = herbarium_url + alphabetical_family_query
+            alphabetical_query = QUERIES["family"] + letter + '%'
+            alphabetical_url = herbarium_url + alphabetical_query
         continue
 
     file_name = OUTPUT_FILE_PREFIX + "_part" + str(i) + ".txt"
-    file_path = OUTPUT_PATH / SITE_NAME / file_name
+    file_path = output_path / site_name / file_name
     with file_path.open(mode='w') as output_file:
         output_file.write(response.text)
 
 if len(empty_herbaria) > 0:
     print("\nSome herbaria did not return CSVs.")
     print("Here is a url to see why:\n")
-    print(region_url + HERBARIA_QUERY + ",".join(empty_herbaria))
+    print(region_url + QUERIES["herbaria"] + ",".join(empty_herbaria))
 
 if len(unknown_error_herbaria) > 0:
     print("\nSome herbaria had unknown errors. Deal with these manually:")
