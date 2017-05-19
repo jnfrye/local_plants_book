877ac4567e51ce40c225acf0a349388c0e79888c
Josh Frye <josh.n.frye@gmail.com>
2017-05-19 11:43:50 -0700

    Implement selections locator in partitioner

--- scripts/observations/analyze/partitioning_determiner.py
+++ scripts/observations/analyze/partitioning_determiner.py
@@ -1,14 +1,20 @@
 import argparse
 import pandas as pd
 
+import PyFloraBook.input_output.data_coordinator as dc
 from PyFloraBook.threshold.partition import find_best_partitioning
 
+
+# ---------------- GLOBALS ----------------
+INPUT_SUFFIX = "scores"
+OUTPUT_SUFFIX = "partitions"
 
 # ---------------- INPUT ----------------
 # Parse arguments
 parser = argparse.ArgumentParser(
     description='Determine the optimal distribution of pages for each genus '
-                'and species (for my plant drawing project).')
+                'and species (for my plant drawing project).'
+    )
 parser.add_argument("family", help="Name of the family to be analyzed.")
 parser.add_argument(
     "total_pages", type=int,
@@ -38,9 +44,16 @@ min_page_per_species = args.min_page_per_species
 family = args.family
 constant_species_pages = args.constant_species_pages
 
+# Locate input/output paths
+scores_folder = dc.locate_scores_folder()
+selections_folder = dc.locate_selections_folder()
+
 # Each species has a score of how important or common it is
+scores_file_name = family + '_' + INPUT_SUFFIX + '.csv'
 species_table = pd.read_csv(
-    './scores/' + family + '_scores.csv', index_col=['genus', 'species'])
+    str(scores_folder / scores_file_name),
+    index_col=['genus', 'species']
+    )
 
 # ---------------- ANALYSIS ----------------
 # If the pages were partitioned exactly in proportion, this would be the result
@@ -71,8 +84,9 @@ for genus in genus_results.index:
     leftover_pages = pages_for_this_genus - \
         temp_results['best_partitioning'].sum()
     if leftover_pages != 0:
-        print("WARNING: Sum of pages for genus' species does not match target "
-              "pages! Excess of", leftover_pages, "pages for genus", genus)
+        print("WARNING: Sum of pages for genus' species does not match "
+              "target pages! Excess of", leftover_pages, "pages for genus",
+              genus)
 
     temp_results.set_index([genus + " " + temp_results.index], inplace=True)
     species_results = species_results.append(temp_results)
@@ -83,6 +97,8 @@ print(genus_results)
 print("\nSPECIES RESULTS\n--------------")
 print(species_results)
 
-genus_results.to_csv('./partitions/' + family + '_genus_partitions.csv')
-species_results.to_csv('./partitions/' + family + '_species_partitions.csv')
+genus_output_name = family + '_genus_' + OUTPUT_SUFFIX + '.csv'
+species_output_name = family + '_species_' + OUTPUT_SUFFIX + '.csv'
+genus_results.to_csv(str(selections_folder / genus_output_name))
+species_results.to_csv(str(selections_folder / species_output_name))
 
