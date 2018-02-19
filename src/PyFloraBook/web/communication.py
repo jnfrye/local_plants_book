e2718a0144dfa634e88b395b523a6668363ac1a0
Josh Frye <josh.n.frye@gmail.com>
2018-02-19 11:30:19 -0800

    Rename import for readability

--- src/PyFloraBook/web/communication.py
+++ src/PyFloraBook/web/communication.py
@@ -1,14 +1,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as expect
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import time
 
 timeout = 10  # seconds
 
+
 def wait_for_load(browser, attribute, class_to_look_for):
     try:
-        element_present = EC.presence_of_element_located(
+        element_present = expect.presence_of_element_located(
             (getattr(By, attribute), class_to_look_for)
             )
         WebDriverWait(browser, timeout).until(element_present)
